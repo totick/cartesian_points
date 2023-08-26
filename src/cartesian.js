f8633a288014f1b0ccf0c0f3ab0f7e948d87a159
John Lundgren <john.paul.lundgren@gmail.com>
2023-08-26 06:45:36 +0200

    Implemented cartesianPointsDistance function.

--- src/cartesian.js
+++ src/cartesian.js
@@ -1,4 +1,10 @@
 export function cartesianPointsDistance(cp1, cp2) {
-  //TODO: calculate the distance between two cartesian points
-  return undefined;
+  try {
+    const x = Math.pow(cp2[0] - cp1[0], 2);
+    const y = Math.pow(cp2[1] - cp1[1], 2);
+    const result = Math.sqrt(x + y);
+    return result;
+  } catch (error) {
+    return undefined;
+  }
 }
